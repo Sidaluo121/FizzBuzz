e3c3f18e9ea0df0ecf15831678e603ca6cdf1a4d
Sida Luo <sida.luo@mail.utoronto.ca>
2025-09-08 20:23:02 -0400

    "refactored to extract helper method"

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -3,7 +3,7 @@ package lab1;
 public class Multiples {
     public static void main(String[] args) {
         int count = 0;
-        count = multiples();
+        count = multiples(1000,3,5);
         System.out.println(count);
     }
 
@@ -13,6 +13,17 @@ public class Multiples {
             Boolean multipleOf3 = i % 3 == 0;
             Boolean multipleOf5 = i % 5 == 0;
             if (multipleOf3 || multipleOf5) {
+                count++;
+            }
+        }
+        return count;
+    }
+    private static int multiples(int n, int a, int b) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            Boolean multipleOfa = i % a == 0;
+            Boolean multipleOfb = i % b == 0;
+            if (multipleOfa || multipleOfb) {
                 count++;
             }
         }
